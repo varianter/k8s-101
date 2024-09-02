e13fd0bb9fec92b673596644f6fd4e6d20ed6c3b
Jonas Bjøralt <61310258+jonasbjoralt@users.noreply.github.com>
2024-09-02 12:28:43 +0200

    Add simple api (#1)
    * add simple api we can build a container from
    
    * add gitignore

--- README.md
+++ README.md
@@ -6,20 +6,29 @@ Sørg for å ha følgende installert:
  - kubectl
 # Del 1: Hallo verden
 
+## 1.1 - Deploy et enkelt API/nettside
  - deploye en applikasjon `k create deploy myapp --image=img`  eller via yaml 
  - Konfigurere med miljøvariabler 
  - Koble på en service og eksponer appen  
  - Skalere opp 
+## 1.2 - Slides om hva vi nettopp gjorde
+ Forklar: 
+  - Nodes, Control Plane 
+  - Deployment, pods 
+  - Services 
 
 # Del 2: Oppdater
+## 2.1 Gjøre ting 
  - Sette på readiness- og livenessProbes
  - Oppdater til neste versjon. Denne krever tilkobling til DB og failer derfor
 	 - Her kan man eventuelt velge å bruke en strategi som canary eller blue/green
  - Rull tilbake. 
  - Gå til neste seksjon -> lage en database
+## 2.2 Slides om åssen deployments endres, replicasets osv 
 
--- Pause? 
-# Del 3:  Legg på en database 
+# Pause 
+# Del 3:  Legg på en database
+// Denne delen kan bli lang - Vi kan vurdere å droppe den. Eller lage noen snarveier kanskje 
 ## 3.1 - Bruk en secret for å legge database-passord, evt connection-string
 
 ## 3.2 - Sett opp persistent volum, claim
@@ -27,5 +36,3 @@ Sørg for å ha følgende installert:
 ## 3.3 - Kjør database i cluster 
 
 ## 3.4 - Oppdater app til å bruke database
-
-
---
+++ ressurser/simple-api/Program.cs
@@ -0,0 +1,55 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseSwagger(); 
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+// This isn't how you usually do this in .NET. See options-patterns instead. 
+var favoriteLunch = Environment.GetEnvironmentVariable("FAV_LUNCH");
+var machineName = Environment.MachineName;
+
+var isHealthy = true;
+
+app.MapGet("/info", () =>
+    {
+        if (isHealthy)
+            return new
+            {
+                favoriteLunch,
+                machineName
+            };
+
+        throw new Exception("Sorry I am broken :(");
+    })
+    .WithName("SystemInfo")
+    .WithOpenApi();
+
+// Boss said we needed this done ASAP
+app.MapPost("/experimental", () =>
+{
+    isHealthy = false;
+    return "I think this went OK";
+}).WithName("BETA: Experimental feature")
+.WithOpenApi();
+
+// Endpoint to probe weather or not this API is working properly
+app.MapGet("/health", () =>
+    {
+        if (isHealthy) return "OK";
+        throw new Exception("OH NO I AM UNHEALTHY");
+    })
+.WithName("Health")
+.WithOpenApi();
+
+// This app requires some time to get started. 
+// Maybe you should use a readiness-probe? 
+Thread.Sleep(3000);
+
+app.Run();
